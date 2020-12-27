ff30555b68d962c5e8cc51f3f8a9bbd50e60150e
princewhyte2 <princewhyte02@gmail.com>
2020-12-27 22:18:50 +0100

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
+
 
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+        <Menu />
+
     </div>
   );
 }
